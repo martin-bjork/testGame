e63240bc4196dcf58fcca1a83c490ab1cf867627
martin-bjork <martin.bjork_92@hotmail.com>
2014-03-15 23:30:46 +0100

    Added a background image

--- view.py
+++ view.py
@@ -1,20 +1,40 @@
+import os
+
 import pygame
 
-WIDTH = 600
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 800
 
 
 def set_up_window():
     pygame.init()
 
     # Initialize the display
-    winstyle = 0            # FULLSCREEN
-    best_color_depth = pygame.display.mode_ok([WIDTH, HEIGHT], winstyle, 32)
-    screen = pygame.display.set_mode([WIDTH, HEIGHT], winstyle,
-                                     best_color_depth)
+    screen = pygame.display.set_mode([WIDTH, HEIGHT])
 
-    # Hide cursor
+    # Decorate window, hide cursor
+    icon = load_image('smiley_small.png')
     pygame.display.set_caption('Test window')
+    pygame.display.set_icon(icon)
     pygame.mouse.set_visible(0)
 
-    # TODO: Add more stuff here (set up background etc)
+    # TODO: Add more stuff here
+
+    # Load background image
+    background_image = load_image('smiley.png')
+    screen.blit(background_image, (0, 0))
+
+    # Draw window
+    pygame.display.flip()
+
+
+def load_image(file_name):
+    "loads an image, prepares it for play"
+    full_name = os.path.join('images', file_name)
+    try:
+        surface = pygame.image.load(full_name)
+    except pygame.error:
+        print 'Could not load image "{name}".\nError message: {message}'\
+            .format(name=file_name, message=pygame.get_error())
+        raise SystemExit
+    return surface.convert()
