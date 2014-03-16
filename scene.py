1508cbc15a58f6020d0710da92cc8bc64297e1c2
martin-bjork <martin.bjork_92@hotmail.com>
2014-03-16 21:07:02 +0100

    Removed unneccessary bodies

--- scene.py
+++ scene.py
@@ -22,17 +22,14 @@ def init_scene(game):
 
     width, height = game.get_screen_size()
 
-    # Create bodies for the world boundaries
-    floor_body = pymunk.Body()
-    ceiling_body = pymunk.Body()
-    right_wall_body = pymunk.Body()
-    left_wall_body = pymunk.Body()
+    # Create a static for the world boundaries
+    static_body = pymunk.Body()
 
     # Create the boundaries
-    floor = pymunk.Segment(floor_body, (0, 0), (width, 0), 5.0)
-    ceiling = pymunk.Segment(ceiling_body, (0, height), (width, height), 5.0)
-    right_wall = pymunk.Segment(right_wall_body, (width, 0), (width, height), 5.0)
-    left_wall = pymunk.Segment(left_wall_body, (0, 0), (0, height), 5.0)
+    floor = pymunk.Segment(static_body, (0, 0), (width, 0), 5.0)
+    ceiling = pymunk.Segment(static_body, (0, height), (width, height), 5.0)
+    right_wall = pymunk.Segment(static_body, (width, 0), (width, height), 5.0)
+    left_wall = pymunk.Segment(static_body, (0, 0), (0, height), 5.0)
 
     # Add them to the space
     space.add(floor)
