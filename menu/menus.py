e118f95be1f5c4046e30fc6ccf812699593107aa
martin-bjork <martin.bjork_92@hotmail.com>
2014-11-16 23:07:19 +0100

    Removed code duplication in menus

--- menu/menus.py
+++ menu/menus.py
@@ -3,8 +3,6 @@ import pygame
 from gameclass import Game as Game
 import load_yaml
 
-# TODO: Minimize code duplication
-
 
 def run_menu(file_path):
 
@@ -15,38 +13,16 @@ def run_menu(file_path):
     clock = pygame.time.Clock()
     FPS = 60
 
-    # A reference to the button that has been pressed
-    # (Needed since if a button has been pressed we want
-    # to break the loop and then perform the buttons event)
-    pressed_button = None
-
     run = True
     while run:
 
-        mouse_pos, clicked, open_pop_up = Game.take_menu_input()
+        run, pressed_button, open_pop_up = menu_loop(buttons, clock, FPS,
+                                                     obj_group, screen)
 
         if open_pop_up:
             run_pop_up_menu('quit_game_pop_up.yaml')
             # Redraw screen to erase pop-up menu
             redraw(screen, background, obj_group)
-
-        if clicked:
-            # Check if a button has been pressed
-            for button in buttons:
-                if button.pressed(mouse_pos):
-                    pressed_button = button
-                    run = False
-                    break
-        else:
-            for button in buttons:
-                button.set_hovered(button.pressed(mouse_pos))
-
-        # Keep the fps down
-        clock.tick(FPS)
-
-        # Redraw the screen
-        dirty_rects = obj_group.draw(screen)
-        pygame.display.update(dirty_rects)
 
     # We have broken out of the loop, check if a button has
     # been pressed; if so, perform its action
@@ -66,40 +42,44 @@ def run_pop_up_menu(file_path):
     clock = pygame.time.Clock()
     FPS = 60
 
-    # A reference to the button that has been pressed
-    # (Needed since if a button has been pressed we want
-    # to break the loop and then perform the buttons event)
-    pressed_button = None
-
     run = True
     while run:
 
-        mouse_pos, clicked, open_pop_up = Game.take_menu_input()
-
-        run = True
-
-        if clicked:
-            # Check if a button has been pressed
-            for button in buttons:
-                if button.pressed(mouse_pos):
-                    pressed_button = button
-                    run = False
-                    break
-        else:
-            for button in buttons:
-                button.set_hovered(button.pressed(mouse_pos))
-
-        # Keep the fps down
-        clock.tick(FPS)
-
-        # Redraw the screen
-        dirty_rects = obj_group.draw(screen)
-        pygame.display.update(dirty_rects)
+        run, pressed_button, open_pop_up = menu_loop(buttons, clock, FPS,
+                                                     obj_group, screen)
 
     # We have broken out of the loop, check if a button has
     # been pressed; if so, perform its action
     if pressed_button is not None:
         pressed_button.perform_action()
+
+
+def menu_loop(buttons, clock, FPS, obj_group, screen):
+
+    mouse_pos, clicked, open_pop_up = Game.take_menu_input()
+
+    run = True
+    pressed_button = None
+
+    if clicked:
+        # Check if a button has been pressed
+        for button in buttons:
+            if button.pressed(mouse_pos):
+                pressed_button = button
+                run = False
+                break
+    else:
+        for button in buttons:
+            button.set_hovered(button.pressed(mouse_pos))
+
+    # Keep the fps down
+    clock.tick(FPS)
+
+    # Redraw the screen
+    dirty_rects = obj_group.draw(screen)
+    pygame.display.update(dirty_rects)
+
+    return run, pressed_button, open_pop_up
 
 
 def redraw(screen, background, obj_group):
